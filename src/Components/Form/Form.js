b68b4c6d51a36c6555a061825aea3ef6d8f647d9
rossifumi46 <32728676+rossifumi46@users.noreply.github.com>
2020-11-01 22:37:38 +0600

    Revert "Form"

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,30 +1,25 @@
 import React from 'react';
 
-import "./__input/form__input.css";
-import "./form.css";
-
-
 const Form = () => {
   return (
-    <div className="form">
-      <h2 className="form__title">Форма</h2>
-      <form action="submit">
-						<div className="form__inputs">
-              <input className="form__input" type="text" placeholder="Имя и фамилия автора" minlength="2" maxlength="30" required/>
-              <input className="form__input" type="email" placeholder="Почта" minlength="5" required/>
-              <input className="form__input" type="tel" placeholder="Телефон" required maxlength="11"/>
-              <textarea className="form__input form__input_textarea" type="textarea" placeholder="Стихи"></textarea>
-						</div>
-						<div class="checkbox">
-							<label>
-								<input id="1" className="invisible-checkbox" type="checkbox"/>
-								<span className="visible-checkbox"></span>
-								<span className="form__label-text">Согласен с офертой</span>
-							</label>
-						</div>
-					<button className="form__submit" type="submit">Отправить</button>
-				</form>
+    <div>
+      <h3>Форма</h3>
+      <form>
+        <input type='text' placeholder='Имя и фамилия автора'/>
+        <span></span>
+        <input type='e-mail' placeholder='Почта'/>
+        <span></span>
+        <input type='text' placeholder='Телефон'/>
+        <span></span>
+        <input type='text' placeholder='Стихи'/>
+
+        <button type='radio'/>
+
+
+      </form>
+
     </div>
+
   )
 }
 
