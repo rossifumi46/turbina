0fbbc10f11b6c7caaa041251400fabf7ae498e81
rossifumi46 <rossifumi.gp@gmail.com>
2020-11-01 21:04:46 +0600

    form styles and about: white part

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="root">
 
+=======
+import Menu from "../Menu/Menu.js";
+import Content from "../Content/Content.js";
+
+import "./../../index.css"
+function App() {
+  return (
+    <div className="root">
+      <Menu/>
+      <Content/>
+>>>>>>> Stashed changes
     </div>
   );
 }
