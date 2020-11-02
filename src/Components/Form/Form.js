aff51c01c8c214af4a7c0eb64de37dc499b613a6
Alexey Shumilin <shumil.alexey@yandex.ru>
2020-11-02 22:55:37 +0200

    form component improve
    some new text syles added
    new style inputs added
    checked checkbox style from new figma added

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -8,6 +8,7 @@ const Form = () => {
   return (
     <div className="form">
       <h2 className="form__title">Форма</h2>
+		<p className="form__ps">Заполняя эту форму, вы становитесь частью проекта.</p>
       <form action="submit">
 						<div className="form__inputs">
               <input className="form__input" type="text" placeholder="Имя и фамилия автора" minlength="2" maxlength="30" required/>
@@ -19,7 +20,7 @@ const Form = () => {
 							<label>
 								<input id="1" className="invisible-checkbox" type="checkbox"/>
 								<span className="visible-checkbox"></span>
-								<span className="form__label-text">Согласен с офертой</span>
+								<span className="form__label-text">Согласен с <a className="form__label-text_link" href="#">офертой</a></span>
 							</label>
 						</div>
 					<button className="form__submit" type="submit">Отправить</button>
