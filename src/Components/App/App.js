568511ce39ef7772d6a6c5cc046044fd7ebdbfe3
rossifumi46 <rossifumi.gp@gmail.com>
2020-11-03 20:28:40 +0600

    footer responsive

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,16 @@
 import React   from "react";
 import Main  from "../Main/Main.js";
 import Content from "../Content/Content.js";
+import Footer from "../Footer/Footer.js";
 
 import "./../../index.css"
 
 function App() {
   return (
     <div className="root">
-      <Main/>
+      {/* <Main/> */}
       <Content/>
+      <Footer/>
     </div>
   );
 }
