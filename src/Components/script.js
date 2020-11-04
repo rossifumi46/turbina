0028b865b67da2ffcec62b3631f97c36a06d697b
rossifumi46 <rossifumi.gp@gmail.com>
2020-11-04 23:13:43 +0600

    player

--- src/Components/script.js
+++ src/Components/script.js
@@ -2,10 +2,21 @@
 
 export function toggleMore() {
   const releasesElement = document.querySelector('.player__releases');
-  const moreElement = document.querySelector('.player__releases');
+  const textElement = document.querySelector('.player__text');
+  const moreElement = document.querySelector('.more__container');
   const moreBtn = document.querySelector('.more');
+  if (!moreElement.classList.contains('more__container_hidden')) {
+    if (releasesElement.classList.contains('player__releases_hidden')) {
+      textElement.classList.toggle('player__text_hidden');
+    } else {
+      releasesElement.classList.toggle('player__releases_hidden');
+    }
+  } else {
+    releasesElement.classList.toggle('player__releases_hidden');
+  }
+
   moreElement.classList.toggle('more__container_hidden');
-  releasesElement.classList.toggle('player__releases_hidden');
+  
   moreBtn.classList.toggle('hide');
 }
 
